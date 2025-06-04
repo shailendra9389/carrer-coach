a2fb77cb5e7104bc503eb1b74bc6db77d5388302
Priyanshu bidhuri <156426411+bidhuripriyanshu@users.noreply.github.com>
2025-06-04 11:53:09 +0530

    third commit with adding logo.png

--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header> 
+      <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt="Carrer Coach Logo"
+            width={200}
+            height={60}
+            className="h-12 py-1 w-auto object-contain"
+          />
+        </Link>
+      <div>
+      <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+      </div>
+
+      
+         </header>
   )
 }
 
