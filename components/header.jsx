f5d06b352ba0d328cfdd55c54711c1fe48f7acf5
{shailendra9389} <sps976130@gmail.com>
2025-06-04 11:58:54 +0530

    Apne changes for header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, UserButton ,SignUpButton} from "@clerk/nextjs";
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div> 
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
   )
 }
 
