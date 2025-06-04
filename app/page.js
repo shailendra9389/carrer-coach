ce4321cb980aa96a3e66970e610d8fd7dd03be7b
{shailendra9389} <sps976130@gmail.com>
2025-06-04 13:31:13 +0530

    logo-Height

--- app/page.js
+++ app/page.js
@@ -4,15 +4,5 @@ import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-  <h1 className="text-6xl font-bold underline text-red-400w">
-      Hello world!
-    </h1>
-   
-   {/* <p className="text-red-500">this is me</p> */}
-    <Button className="">Hello</Button>
-    <Input className="w-30px"></Input>
-    </div>
-  );
+  
 }
