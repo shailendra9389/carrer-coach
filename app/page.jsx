872d8cafd272babb81d3a08f5282b84c06c429a6
Priyanshu bidhuri <156426411+bidhuripriyanshu@users.noreply.github.com>
2025-06-05 12:13:03 +0530

    final-working-front-page

--- app/page.jsx
+++ app/page.jsx
@@ -212,16 +212,4 @@ export default function LandingPage() {
   );
 }
 
-import HeroSection from "@/components/hero.jsx";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import Image from "next/image";
 
-export default function Home() {
-  return(
-    <div>
-        <div className="grid-background"></div>
-        <HeroSection/>
-    </div>
-  )
-}
