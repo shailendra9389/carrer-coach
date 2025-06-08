a48a28cff4f2a9119c591e8e346dd32370439c16
{shailendra9389} <sps976130@gmail.com>
2025-06-08 09:54:22 +0530

    onboarding by sp

--- actions/user.js
+++ actions/user.js
@@ -35,9 +35,9 @@ export async function updateUser(data) {
               industry: data.industry,
               salaryranges: [],
               growthrate: 0,
-              demandLevel: "Medium",
+              demandLevel: "MEDIUM",
               topSkills: [],
-              marketOutlook: "Neutral",
+              marketOutlook: "NEUTRAL",
               keyTrends: [],
               recommendedSkills: [],
               nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
@@ -66,7 +66,7 @@ export async function updateUser(data) {
     );
 
     // revalidatePath("/");
-    return result.user;
+    return { success: true, ...result };
   } catch (error) {
     console.error("Error updating user and industry:", error.message);
     throw new Error("Failed to update profile");
