770204b7b845dc250573a50ddc5c91963d57c005
Priyanshu bidhuri <156426411+bidhuripriyanshu@users.noreply.github.com>
2025-06-08 09:19:06 +0530

    adding hooks folder

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,6 +1,19 @@
-const OnbordingPage = () => {
-    
-    return <h1>Onboarding</h1>
-}   
+import { redirect } from "next/navigation";
+import { industries } from "@/data/industries";
+import OnboardingForm from "./_components/onboarding-form";
+import { getUserOnboardingStatus } from "@/actions/user";
 
-export default OnbordingPage
+export default async function OnboardingPage() {
+  // Check if user is already onboarded
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  if (isOnboarded) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <main>
+      <OnboardingForm industries={industries} />
+    </main>
+  );
+}
